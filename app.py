0ab8bce4384466d6e6c4d85b31670efb29b6ec43
QuickZam <111820344+QuickZam@users.noreply.github.com>
2022-10-02 07:28:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -150,16 +150,16 @@ if choice == 'Login':
 
         with C: 
           if selected == 'Contact': 
-          st.header(":mailbox: Get Touch With Us!")
-          contact_form = """
-          <form action="https://formsubmit.co/quickzam.ai@gmail.com" method="POST">
-               <input type="hidden" name="_captcha" value="false">
-               <input type="text" name="name" placeholder="Your name" required>
-               <input type="email" name="email" placeholder="Your email" required>
-               <textarea name="message" placeholder="Your message here"></textarea>
-               <button type="submit">Send</button>
-          </form>
-          """
+            st.header(":mailbox: Get Touch With Us!")
+            contact_form = """
+            <form action="https://formsubmit.co/quickzam.ai@gmail.com" method="POST">
+                 <input type="hidden" name="_captcha" value="false">
+                 <input type="text" name="name" placeholder="Your name" required>
+                 <input type="email" name="email" placeholder="Your email" required>
+                 <textarea name="message" placeholder="Your message here"></textarea>
+                 <button type="submit">Send</button>
+            </form>
+            """
           st.markdown(contact_form, unsafe_allow_html=True)
           def local_css(file_name):
             with open(file_name) as f:
