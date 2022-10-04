3037cc6fe8529b67c6891d94803b2923114fb271
QuickZam <111820344+QuickZam@users.noreply.github.com>
2022-10-04 14:43:35 +0530

    Update playlist.py

--- playlist.py
+++ playlist.py
@@ -115,7 +115,9 @@ class DFMaker:
         link.append(i['link'])
       n_3_grams = self.generate_N_grams(text,3)
 
-      total_emb = self.bi_encoder.encode(n_3_grams, convert_to_numpy = True)
+      #total_emb = self.bi_encoder.encode(n_3_grams, convert_to_numpy = True)
+      for sen in n_3_grams:
+        total_emb.append(self.bi_encoder.encode(sen,convert_to_numpy=True))
 
       df = pd.DataFrame()
       df['Sentance_3_cleaned'] = n_3_grams
