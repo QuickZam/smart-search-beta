c96c79e6121700198c9794720ecea4346ae71e86
QuickZam <111820344+QuickZam@users.noreply.github.com>
2022-10-05 07:08:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -199,64 +199,71 @@ import streamlit as st
 import urllib.request, urllib
 import streamlit as st, os, json, re   
 from streamlit_lottie import st_lottie  
-from streamlit_option_menu import option_menu  ##
+from streamlit_option_menu import option_menu  
 
 from utils import load_lottie, give_it_here
 from playlist import SemanticSearch  
 
 
 def super(query, dataframe): 
-  se = SemanticSearch(query, dataframe)
-  timing, yt_links = se.search()
-  for i in range(len(yt_links)): 
-    st.caption(f"Video: {i + 1}")
-    st.video(yt_links[i], start_time = timing[i])
+    se = SemanticSearch(query, dataframe)
+    timing, yt_links = se.search()
+    for i in range(len(yt_links)): 
+        st.caption(f"Video: {i + 1}")
+        st.video(yt_links[i], start_time = timing[i])
 
 @st.cache(show_spinner = False)
-def si(): 
-  from single import DFMaker, SingleFileTranscript
-  single_transcript = SingleFileTranscript(link).get_transcript() 
-  single_dataframe = DFMaker(single_transcript).df()
-  st.session_state.single_dataframe = single_dataframe 
-  return single_dataframe
+def si(link): 
+    try: 
+        from single import DFMaker, SingleFileTranscript
+        single_transcript = SingleFileTranscript(link).get_transcript() 
+        single_dataframe = DFMaker(single_transcript).df()
+        st.session_state.single_dataframe = single_dataframe 
+        return single_dataframe
+    except Exception as E: 
+        st.write(E)
 
-
+@st.cache(show_spinner = False)
 def pi(link): 
-  from playlist import DFMaker, TranscriptSaverPlaylist 
-  playlist_transcript = TranscriptSaverPlaylist([link]).get_transcript()
-  st.write(playlist_transcript)
-  playlist_dataframe = DFMaker(playlist_transcript).df()
-  st.write(playlist_dataframe)
-  return playlist_dataframe
+    try: 
+        from playlist import DFMaker, TranscriptSaverPlaylist 
+        playlist_transcript = TranscriptSaverPlaylist([link]).get_transcript()
+        playlist_dataframe = DFMaker(playlist_transcript).df()
+        return playlist_dataframe
+    except Excpetion as E: 
+        st.write(E)
 
 @st.cache(show_spinner = False)
 def ci(): 
-  from channel import DFMaker, TranscriptSaverChannel
-  channel_transcripts = TranscriptSaverChannel(link).get_transcript()
-  channel_dataframe = DFMaker(channel_transcripts).df()
-  st.session_state.single_dataframe = channel_dataframe 
-  return channel_dataframe 
+    try: 
+        from channel import DFMaker, TranscriptSaverChannel
+        channel_transcripts = TranscriptSaverChannel(link).get_transcript()
+        channel_dataframe = DFMaker(channel_transcripts).df()
+        st.session_state.single_dataframe = channel_dataframe 
+        return channel_dataframe 
+    except Exception as E: 
+        st.write(E)
 
 def check_email(email):
-  regex = r'\b[A-Za-z0-9._%+-]+@gmail.com'
-  if(re.fullmatch(regex, email)):
-    return True 
-  else: return False
+    regex = r'\b[A-Za-z0-9._%+-]+@gmail.com'
+    if(re.fullmatch(regex, email)):
+        return True 
+    else: return False
 
 
 def get_title_helper(video_id): 
-  # https://stackoverflow.com/questions/59627108/retrieve-youtube-video-title-using-api-python
+    # https://stackoverflow.com/questions/59627108/retrieve-youtube-video-title-using-api-python
 
-  VideoID = video_id 
-  params = {"format": "json", "url": "https://www.youtube.com/watch?v=%s" % VideoID}
-  url = "https://www.youtube.com/oembed"
-  query_string = urllib.parse.urlencode(params)
-  url = url + "?" + query_string
-  with urllib.request.urlopen(url) as response:
-      response_text = response.read()
-      data = json.loads(response_text.decode())
-      
-  return data['title']
+    VideoID = video_id 
+    params = {"format": "json", "url": "https://www.youtube.com/watch?v=%s" % VideoID}
+    url = "https://www.youtube.com/oembed"
+    query_string = urllib.parse.urlencode(params)
+    url = url + "?" + query_string
+    with urllib.request.urlopen(url) as response:
+        response_text = response.read()
+        data = json.loads(response_text.decode())
+        
+    return data['title']
 
 ########################### Page Configuraiton ###############################
 page_title = "Smart Search"
@@ -301,38 +308,38 @@ email = st.sidebar.text_input("Please enter your email address")
 
 # checking email is proper or not! 
 if email: 
-  if check_email(email) == False:  
-    st.error('Please enter valid email address!')
-    st.stop()
+    if check_email(email) == False:  
+        st.error('Please enter valid email address!')
+        st.stop()
 
 password = st.sidebar.text_input("please enter your password", type = 'password')
 
 
 # sign up configuration 
 if choice == 'Sign up': 
-  handle = st.sidebar.text_input("Please input your handle name", placeholder = 'Enter your username')
-  submit = st.sidebar.button('Create my account!')
+    handle = st.sidebar.text_input("Please input your handle name", placeholder = 'Enter your username')
+    submit = st.sidebar.button('Create my account!')
 
-  if submit: 
-    # handling user sign up
-    try: 
-      user = auth.create_user_with_email_and_password(email, password)  # creating a account with username and password 
-      st.success("Your account is created succesfully!")
+    if submit: 
+        # handling user sign up
+        try: 
+            user = auth.create_user_with_email_and_password(email, password)  # creating a account with username and password 
+            st.success("Your account is created succesfully!")
 
-    except requests.HTTPError as exception:
-      if "WEAK_PASSWORD" in str(exception): 
-        st.info("Your password is too weak. Try different password")
-        st.markdown("If you have doubt in creating strong password. Visit this [website](https://1password.com/password-generator/)", True)
-        st.stop()
+        except requests.HTTPError as exception:
+            if "WEAK_PASSWORD" in str(exception): 
+                st.info("Your password is too weak. Try different password")
+                st.markdown("If you have doubt in creating strong password. Visit this [website](https://1password.com/password-generator/)", True)
+                st.stop()
 
-    # sign up 
-    user = auth.sign_in_with_email_and_password(email, password)
+        # sign up 
+        user = auth.sign_in_with_email_and_password(email, password)
 
-    db.child(user['localId']).child("Handle").set(handle)
-    db.child(user['localId']).child("ID").set(user['localId'])
-    
-    st.title("Welcome " + handle)
-    st.info("Your account succesfully created. Now you can login!")
+        db.child(user['localId']).child("Handle").set(handle)
+        db.child(user['localId']).child("ID").set(user['localId'])
+        
+        st.title("Welcome " + handle)
+        st.info("Your account succesfully created. Now you can login!")
 
 if choice == 'Login':
     login = st.sidebar.checkbox('Login')
@@ -367,65 +374,82 @@ if choice == 'Login':
                 st.error("Check the link") 
 
             if st.button('Start the Engine!'): 
+                if st.session_state.result == 'SINGLE': 
+                    with st.spinner('This will take some time!'):
+                        try: 
+                            single_dataframe = si(link)
+                            title =  get_title_helper(link.split('=')[1])
+                            title = re.sub(r'[^a-zA-Z]', ' ', title)
 
-              if st.session_state.result == 'SINGLE': 
-                with st.spinner('This will take some time!'):
-                  single_dataframe = si()
-                  title =  get_title_helper(link.split('=')[1])
+                            dict_single_dataframe = single_dataframe.to_json()
+                            db.child(user['localId']).child("DF").child(title).push(dict_single_dataframe)  
 
-                  dict_single_dataframe = single_dataframe.to_json()
-                  db.child(user['localId']).child("DF").push(dict_single_dataframe)
-                  db.child(user['localId']).child('LINK').push(title)
-                  st.success('Your file succesfully uploaded to database!')
-                
-              if st.session_state.result == 'CHANNEL': 
-                with st.spinner('This will take some time!'):
-                  single_dataframe = ci()
+                            st.success('Your file succesfully uploaded to database!')
 
-                  dict_single_dataframe = single_dataframe.to_json()
-                  db.child(user['localId']).child("DF").push(dict_single_dataframe)
-                  st.success('Your file succesfully uploaded to database!')
+                        except Exception as E: 
+                            st.write(E)
+                    
+                if st.session_state.result == 'CHANNEL': 
+                    with st.spinner('This will take some time!'):
+                        try: 
+                            single_dataframe = ci()
+                            title =  Channel(link).channel_name
+                            title = re.sub(r'[^a-zA-Z]', ' ', title)
 
+                            dict_single_dataframe = single_dataframe.to_json()
+                            db.child(user['localId']).child("DF").child(title).push(dict_single_dataframe) 
+                            st.success('Your file succesfully uploaded to database!')
 
-              if st.session_state.result == 'PLAYLIST': 
-                with st.spinner('This will take some time!'): 
-                    single_dataframes = pi(link)
-                    title =  Playlist(link).title
-                    dict_single_dataframes = single_dataframes.to_json()
+                        except Exception as E: 
+                            st.write(E)
+                    
 
+                if st.session_state.result == 'PLAYLIST': 
+                    with st.spinner('This will take some time!'): 
+                        try: 
+                            single_dataframes = pi(link)
+                            title =  Playlist(link).title
+                            title = re.sub(r'[^a-zA-Z]', ' ', title)
 
-                    db.child(user['localId']).child("DF").push(dict_single_dataframes)
-                    db.child(user['localId']).child('LINK').push(title)
-                    st.success('Your file succesfully uploaded to database!')
+                            dict_single_dataframes = single_dataframes.to_json()
+                            db.child(user['localId']).child("DF").child(title).push(dict_single_dataframes) 
+                            st.success('Your file succesfully uploaded to database!') ##
+
+                        except Exception as E: 
+                            st.write(E)
 
           if op ==  'Use existing video from database': 
 
             try: 
-              list_of_link = list(db.child(user['localId']).child("LINK").get())
               list_of_files = list(db.child(user['localId']).child('DF').get())
             
               list_ = []
-              for i in list_of_link: 
-                list_.append(i.val())
+              for i in list_of_files: 
+                list_.append(i.key())
               option = st.selectbox('Select the video to Query', list_)
+
               index_ = list_.index(option)
 
               list_f = []
               for b in list_of_files: 
                 list_f.append(b.val())
 
+              a = list(list_f[index_].keys())
+              js = list_f[index_].get(a[0])
+
               qyery = st.text_area('Enter your Query')
-              df = pd.read_json(list_f[index_])
+              df = pd.read_json(js)
               df['Embeddings'] = df['Embeddings'].apply(lambda x: np.array(x, dtype = np.float32 ))
              
 
-              if st.button('process'):  ##
+              if st.button('process'): 
                 try: 
                   super(qyery, df)
                 except NameError: 
                   st.error("Try to toggle on Start Engine Button! This is because you off the start engine button!") ### so good
             except Exception as E: 
-              st.info('Upload Some links!')
+                st.write(E)
+                st.info('Upload Some links!')
 
     except Exception as E:
       go = str(E)
@@ -438,4 +462,4 @@ if choice == 'Login':
       if "EMAIL_NOT_FOUND" in go:
         st.error('Your email is not found')
         st.caption("Try to create a email in sign up page and try again") 
-        st.stop()
+        st.stop() #### this is godd! 
